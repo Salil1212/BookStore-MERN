7c97afb930705ca88118049109795a7cf07c1286
Salil1212 <salilnigam1212@gmai.com>
2024-07-01 17:53:59 +0530

    last update

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,7 @@ app.use(express.json());
 // CORS configuration
 
 const corsOptions = {
-  origin:
-    "https://book-store-mern-client-7xklvg58b-salil1212s-projects.vercel.app/", // Allow your frontend origin
+  origin: "https://book-store-mern-frontent-client.vercel.app/", // Allow your frontend origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 204,
 };
